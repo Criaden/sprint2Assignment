f600d145636080b5c343335099f75f419e686f0c
spence369 <45887281+spence369@users.noreply.github.com>
2019-11-21 09:06:02 -0600

    Add files via upload

--- Assignment_2/search.php
+++ Assignment_2/search.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once("Template.php");
 
 $page = new Template("Home");
@@ -11,14 +11,27 @@ $page->finalizeBottomSection();
 print $page->getTopSection();
 
 		print "<div class= \"top\">";
-			print "<h1> Assignment 1 - CIS 310 </h1>";
+			print "<h1> Sprint 3 - CIS 310 </h1>";
 	
 			print "<div class=\"topnavbar\">";
 				print "<ul>";
 					print "<li><a href=\"index.php\">Home</a></li>";
-					print "<li><a href=\"survey.php\" class= 'active'>Survey</a></li>";
+					print "<li><a href=\"survey.php\">Survey</a></li>";
 					print "<li><a href=\"privacy.php\">Privacy</a></li>";
-					print "<li><a href=\"search.php\">Search</a></li>";
+					print "<li><a href=\"search.php\" class= 'active'>Search</a></li>";
+					if(isset($_SESSION['role'])){
+						if($_SESSION['role'] == 'admin'){
+							print "<li><a href=\"admin.php\">Admin</a></li>";
+						}
+					}
+					if(isset($_SESSION['username'])){
+						print "<li><a href=\"logout.php\">Logout</a></li>";
+					}else{
+						print "<li><a href=\"login.php\">Login</a></li>";
+					}
+					if(isset($_SESSION['username'])){
+						print "<li>Welcome, ".$_SESSION['username']."</li>";
+					}
 				print "</ul>";
 			print "</div>";
 		print "</div>";
